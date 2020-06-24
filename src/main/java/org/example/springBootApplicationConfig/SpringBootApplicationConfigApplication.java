b6ab3bc91317cd4e7dddd2b2688ec30d4bc002b4
Murat Kirkoroglu <m.kirkoroglu@web.de>
2020-06-24 12:00:32 +0200

    first commit

---
+++ src/main/java/org/example/springBootApplicationConfig/SpringBootApplicationConfigApplication.java
@@ -0,0 +1,13 @@
+package org.example.springBootApplicationConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springBootApplicationConfig/SpringBootApplicationConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springBootApplicationConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
